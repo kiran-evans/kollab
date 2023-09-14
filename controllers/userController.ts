c7530f5f5794fdf797297ede8ae0b35764e7b367
kiran-evans <thekiranevans@gmail.com>
2023-09-14 12:58:19 +0100

    Added CRUD methods for User

---
+++ controllers/userController.ts
@@ -0,0 +1,69 @@
+import { RequestHandler } from "express";
+import { User } from "../models/User";
+
+export const createUser = (async (req, res) => {
+    try {
+        const user = await User.authenticate(req.params.idToken);
+
+        return res.status(201).json(user.toJSON());
+
+    } catch (err: any) {
+        res.statusMessage = err;
+        res.status(500).send();
+        console.error(err);
+    }
+}) satisfies RequestHandler;
+
+export const getUser = (async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+
+        if (!user) return res.status(404).send();
+
+        return res.status(200).json(user.toJSON());
+
+    } catch (err: any) {
+        res.statusMessage = err;
+        res.status(500).send();
+        console.error(err);
+    }
+}) satisfies RequestHandler;
+
+export const updateUser = (async (req, res) => {
+    try {
+        const user = await User.authenticate(req.body.idToken);
+        const [affectedCount, affectedRows] = await User.update({
+            ...req.body
+        }, {
+            where: {
+                id: user.id
+            },
+            returning: true
+        });
+
+        return res.status(200).send(affectedRows[0].toJSON());
+
+    } catch (err: any) {
+        res.statusMessage = err;
+        res.status(500).send();
+        console.error(err);
+    }
+}) satisfies RequestHandler;
+
+export const deleteUser = (async (req, res) => {
+    try {
+        const user = await User.authenticate(req.body.idToken);
+        await User.destroy({
+            where: {
+                id: user.id
+            }
+        });
+
+        return res.status(204).send();
+
+    } catch (err: any) {
+        res.statusMessage = err;
+        res.status(500).send();
+        console.error(err);
+    }
+}) satisfies RequestHandler;
--- routers/userRouter.ts
+++ routers/userRouter.ts
@@ -1,25 +1,28 @@
 import { Router } from "express";
 import { body, param } from "express-validator";
+import { createUser, deleteUser, getUser, updateUser } from "../controllers/userController";
 
 const router = Router();
 
+// Only the user themselves can perform PUT or DELETE on their data
 router.route('/')
     .post(
-        body('idToken').notEmpty().isJWT()
+        body('idToken').notEmpty().isJWT(),
+        createUser
+    )
+    .put(
+        body('idToken').notEmpty().isJWT(),
+        updateUser
+    )
+    .delete(
+        body('idToken').notEmpty().isJWT(),
+        deleteUser
     );
 
 router.route('/:id')
-    .all(
-        param('id').notEmpty().isUUID()
-    )
-    .put(
-        body('idToken').notEmpty().isJWT()
-    )
-    .patch(
-        body('idToken').notEmpty().isJWT()
-    )
-    .delete(
-        body('idToken').notEmpty().isJWT()
+    .get(
+        param('id').notEmpty().isUUID(),
+        getUser
     );
 
 export default router;
