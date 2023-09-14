145e50b3b8ddf6fff442b6f89c524bdfb0aac033
Sironi-00 <00sironi@gmail.com>
2023-09-14 16:12:22 +0100

    inital Nav-bar

---
+++ client/src/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+function Search() {
+  return (
+    <>
+    <div className="search">
+            <input type="text" name='search' placeholder='search' />
+        </div>
+    </>
+  )
+}
+
+export default Search
