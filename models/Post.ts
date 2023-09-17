e98ceb07e2a96059456f762bb7edb2ac9e4f5c2e
kiran-evans <thekiranevans@gmail.com>
2023-09-17 14:16:14 +0100

    Added documentation for Post

--- models/Post.ts
+++ models/Post.ts
@@ -1,6 +1,17 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../lib/pg";
 
+/**
+ * @type {Object} Post
+ * @property {string} id - The UUID of this Post
+ * @property {string} author_id - The UUID of the author User of this Post
+ * @property {string} title - The title of this Post
+ * @property {string} body - The stringified markdown body of this Post
+ * @property {string[]} upvotes - UUIDs of the Users who upvoted this Post
+ * @property {string[]} downvotes - UUIDs of the Users who downvoted this Post
+ * @property {string[]} tools - UUIDs of the Tools which this Post is tagged with
+ * @property {Difficulty} difficulty - The Difficulty rating of this Post
+ */
 export class Post extends Model {
     declare id: string;
     declare author_id: string;
@@ -13,6 +24,9 @@ export class Post extends Model {
     declare difficulty: Difficulty;
 }
 
+/**
+ * The possible level of effort required
+ */
 export enum Difficulty {
     'Beginner',
     'Intermediate',
