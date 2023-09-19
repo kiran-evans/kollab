233994348fa13cc63ab89a2da3cc2304c8078cc7
kiran-evans <thekiranevans@gmail.com>
2023-09-19 21:01:15 +0100

    Added api calls to the client

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,10 +1,57 @@
-export default function Register () {
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { FormEvent, useContext, useState } from "react";
+import { signUpUser } from "../../api/userApi";
+import { AppContext } from "../../lib/ContextProvider";
+import { fb } from "../../lib/firebase";
+
+export default function Register() {
+    
+    const initialFormEntries = {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    }
+    const [formEntries, setFormEntries] = useState(initialFormEntries);
+
+    const { dispatch } = useContext(AppContext);
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        // If the passwords don't match, cancel this submission
+        if (formEntries.password !== formEntries.confirmPassword) {
+            alert("Passwords must match.");
+            return;
+        }
+        
+        // Create the user in Firebase Auth
+        // This automatically updates the Firebase state with a now logged-in User
+        await createUserWithEmailAndPassword(fb.auth, formEntries.email, formEntries.password);
+
+        // If Firebase Auth failed to create or login the user, cancel this submission
+        if (!fb.auth.currentUser) {
+            throw "Firebase Auth failed to create user.";
+        }
+        
+        const res = await signUpUser(formEntries.username, await fb.auth.currentUser.getIdToken());
+        dispatch({
+            type: 'SET_USER',
+            payload: await res.json()
+        });
+
+        setFormEntries(initialFormEntries);
+    }
+
     return (
         <>
-        <form>
+        <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>Register</legend>
 
+                    <label htmlFor="email">
+                        Email:
+                        <input type="email" name="email" id="email" />
+                    </label>
                     <label htmlFor="username">
                         Username:
                         <input type="text" name="username" id="username" />
@@ -14,7 +61,7 @@ export default function Register () {
                         <input type="password" name="password" id="password" />
                     </label>
                     <label htmlFor="confirm-password">
-                        Confirm-Password:
+                        Confirm Password:
                         <input type="password" name="confirm-password" id="confirm-password" />
                     </label>
                 <input type="submit" value="Submit" />
