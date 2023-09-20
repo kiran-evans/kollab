58ca67bdc35256a7ca6c7a7eb346f01d44bb7900
kiran-evans <thekiranevans@gmail.com>
2023-09-20 19:38:22 +0100

    BrowserRouter now uses app state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,60 +1,38 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
+import { useContext } from 'react'
 import './App.css'
 import Root from './components/Root/Root'
-import Posts from './pages/posts/Posts'
-import Login from './pages/login/Login'
-import Register from './pages/register/Register'
-import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
-import Comments from './pages/comments/Comments'
-import UserPreferences from './pages/userPreferences/UserPreferences'
+import { AppContext } from './lib/ContextProvider'
 import NewPost from './pages/NewPost/NewPost'
-
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children:[
-      {
-        path: '/',
-        element:<ProtectedRoutes />,
-        children: [
-          {
-            path: '',
-            element: <Posts />
-          }, {
-            path: '/new-post',
-            element: <NewPost />
-          }, {
-            path: 'comments',
-            element: <Comments />
-          }, {
-            path: '/preference',
-            element: <UserPreferences />
-          }
-        ]
-      }, {
-        path: '/login',
-        element: <Login />
-      }, {
-        path: '/register',
-        element: <Register />
-      },
-      {
-        path: '*',
-        element: <h1>404 Page Not found</h1>
-      } 
-    ],
-  }
-])
+import Comments from './pages/comments/Comments'
+import Login from './pages/login/Login'
+import Posts from './pages/posts/Posts'
+import Register from './pages/register/Register'
+import UserPreferences from './pages/userPreferences/UserPreferences'
 
 function App() {
-  document.title = "Collab-Rate"
-  return (
-    <>
-    <RouterProvider router={routes} />
-    </>
-  )
+
+    const { state } = useContext(AppContext);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Root />
+
+                    <Route path="" element={<Posts />} />
+                    <Route path="new-post" element={<NewPost />} />
+                    <Route path="comments" element={<Comments />} />
+                    <Route path="preferences" element={state.user ? <UserPreferences /> : <Navigate to="/login" />} />
+                </Route>
+
+                <Route path="/login" element={state.user ? <Navigate to="/" /> : <Login />} />
+                <Route path="/register" element={state.user ? <Navigate to="/" /> : <Register />} />
+                <Route path="*" element={<h1>Page Not Found</h1>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
