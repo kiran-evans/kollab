dbcec5f38acf59a0b9c0e339f4b8946cacd0578f
kiran-evans <thekiranevans@gmail.com>
2023-10-02 13:00:08 +0100

    Posts can now be loaded in a set number at a time

--- client/src/components/PostList/PostList.tsx
+++ client/src/components/PostList/PostList.tsx
@@ -1,20 +1,29 @@
 import { CircularProgress } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { Post } from '../../../types/Post'
+import { getAllPosts } from '../../api/postApi'
 import PostCard from './PostCard/PostCard'
 import './Posts.css'
 
 export const PostList = () => {
     const [postList, setPostList] = useState(new Array<Post>());
-    const [isFetching, setIsFetching] = useState(false);
+    const [isFetching, setIsFetching] = useState(true);
     
     useEffect(() => {
         // Load the posts on page load
+        (async () => {
+            await loadMorePosts(10);
+        })();
     }, []);
+
+    const loadMorePosts = async (numPostsToLoad: number) => {
+        setIsFetching(true);
+        setPostList([...postList, ...await getAllPosts(numPostsToLoad, postList.length - 1)]);
+        setIsFetching(false);
+    }
 
     return (
         <div className="postList">
-            {isFetching && <p><CircularProgress size={15} />&nbsp;Loading...</p>}
             {postList.length ?
                 postList.map(post => (
                     <PostCard key={post.id} data={post} />
@@ -22,6 +31,7 @@ export const PostList = () => {
                 :
                 <p>No posts</p>
             }
+            {isFetching ? <CircularProgress /> : <button onClick={() => loadMorePosts(10)}>Load more posts</button>}
         </div>
     )
 }
